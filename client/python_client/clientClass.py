d21396a7c4f220708234ac7da9bc72bec517cf27
Pandicon <70060103+Pandicon@users.noreply.github.com>
2021-11-04 19:51:14 +0100

    Fixed the mess in client receiving data

--- client/python_client/clientClass.py
+++ client/python_client/clientClass.py
@@ -3,20 +3,20 @@ import socket
 from packet import Packet
 
 class Client():
-    def __init__(self, port, server_ip, disconnect_message, server_address) -> None:
-        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.port = port
-        self.server_ip = server_ip
-        self.disconnect_message = disconnect_message
-        self.server_address = server_address
-        self.client.connect(self.server_address)
+	def __init__(self, port, server_ip, disconnect_message, server_address) -> None:
+		self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.port = port
+		self.server_ip = server_ip
+		self.disconnect_message = disconnect_message
+		self.server_address = server_address
+		self.client.connect(self.server_address)
 
-    def send(self, message: str, messageType: int = 255) -> None:
-        packet = Packet(messageType, message)
-        packed = packet.pack()
-        self.client.send(packed)
+	def send(self, message: str, messageType: int = 255) -> None:
+		packet = Packet(messageType, message)
+		packed = packet.pack()
+		self.client.send(packed)
 
-    def receive(self, length: int = 1024) -> tuple:
-        received_packet = self.client.recv(length)
-        packet = Packet()
-        return packet.unpack(received_packet)
+	def receive(self, length: int = 1024) -> tuple:
+		received_packet = self.client.recv(length)
+		pckt = Packet.unpack(received_packet)
+		return pckt.type, pckt.extract()
