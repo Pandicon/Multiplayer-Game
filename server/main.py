cecd40fa15daf2630267fcf8df23074059442def
bipentihexium <bipentihexium@gmail.com>
2021-12-02 19:16:13 +0100

    Move server into new file

--- server/main.py
+++ server/main.py
@@ -1,13 +1,13 @@
-import socket
 import threading
 
 from board import Board
 from packet import Packet
 from configHandler import loadConfigData
+from server import Server
 from utils import getPublicIP, getLocalIP
 
 def main():
-	global board
+	global board, server
 	board = Board()
 	board.generateBoard()
 	mainConfig = loadConfigData("../config.json")
@@ -22,49 +22,42 @@ def main():
 	else:
 		print("Unknown server mode! (", serverMode, ")")
 		return
-	SERVER_ADDRESS = (SERVER_IP, PORT)
-
-	server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	server.bind(SERVER_ADDRESS)
-
+	server = Server(SERVER_IP, PORT)
+	server.conCallback = clientConCallback
+	server.clientThrCallback = recvFromClient
+	server.disconCallback = clientDisconCallback
 	print("[STARTING] Server is starting...")
-
-	server.listen()
 	print("[LISTENING] Server is listening on IP " + str(SERVER_IP) + " and port " + str(PORT))
-
-	while True:
-		connection, address = server.accept()
-		thread = threading.Thread(target=handleClient, args=(connection, address))
-		thread.start()
-		print("[ACTIVE CONNECTIONS] " + str(threading.activeCount() - 1))
-
-def handleClient(connection: socket.socket, address):
-	print("[NEW CONNECTION] " + str(address) + " connected.")
-	
-	send(connection, board.getWalls(), 1) # walls
-
+	server.acceptClients()
+def clientConCallback(sck, address, id):
+	print("[NEW CONNECTION] " + str(address) + " connected with ID " + str(id) + ".")
+	print("[ACTIVE CONNECTIONS] " + str(threading.activeCount() - 1))
+def recvFromClient(sck, address, id):
+	send(sck, 1, board.getWalls()) # walls
 	connected = True
 	while connected:
-		messageType, message = receive(connection, 1024)
-		if messageType == 0:
+		messageType, message = receive(sck, 1024)
+		if messageType == 0: # disconnect
 			print("[DISCONNECT] Message to disconnect received, disconnecting " + str(address))
 			connected = False
-			#send(connection, "Disconnecting due to such request", 255)
 			break
-
-		print("[" + str(address) + "] " + str(message))
-		send(connection, "Message received", 255)
-
-	connection.close()
-
-def send(connection: socket.socket, message, messageType: int):
-	packet = Packet(messageType, message)
-	connection.send(packet.pack())
-
-def receive(connection: socket.socket, length: int):
-	received_packet = connection.recv(length)
+		elif messageType == 255:
+			print("[CHAT] Message from [" + str(address) + "]: " + message)
+			broadcast(255, message)
+		else:
+			print("received unknown packet from [" + str(address) + "]: " + str(message))
+def send(sck, messageType, message):
+	p = Packet(messageType, message)
+	server.send(sck, p.pack())
+def broadcast(messageType, message):
+	p = Packet(messageType, message)
+	server.broadcast(p.pack())
+def receive(sck, length: int):
+	received_packet = sck.recv(length)
 	pckt = Packet.unpack(received_packet)
 	return pckt.type, pckt.extract()
-
+def clientDisconCallback(sck, address, id):
+	print("[DISCONNECTION] " + str(address) + "(ID " + str(id) + ") disconnected.")
+	print("[ACTIVE CONNECTIONS] " + str(threading.activeCount() - 2))
 if __name__ == "__main__":
 	main()
---
+++ server/server.py
@@ -0,0 +1,41 @@
+import socket
+import threading
+
+class Server:
+	def __init__(self, ip, port):
+		self.sck = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.sck.bind((ip, port))
+		self.sck.listen()
+		self.conCallback = None
+		self.clientThrCallback = None
+		self.disconCallback = None
+		self.clients = { }
+		self.nextClID = 0
+	def acceptClients(self):
+		while True:
+			sck, address = self.sck.accept()
+			if self.conCallback is not None:
+				self.conCallback(sck, address, self.nextClID)
+			thr = threading.Thread(target=self.clientThr, args=(sck, address, self.nextClID))
+			self.nextClID += 1
+			thr.start()
+	def clientThr(self, sck, address, id):
+		self.clients[id] = (sck, address)
+		try:
+			if self.clientThrCallback is not None:
+				self.clientThrCallback(sck, address, id)
+		except Exception as e:
+			del self.clients[id]
+			sck.close()
+			raise e
+		if self.disconCallback is not None:
+			self.disconCallback(sck, address, id)
+		del self.clients[id]
+		sck.close()
+	def send(self, client, data):
+		if type(client) == int:
+			self.clients[client][0].send(data)
+		client.send(data)
+	def broadcast(self, data):
+		for id, cl in self.clients.items():
+			cl[0].send(data)
